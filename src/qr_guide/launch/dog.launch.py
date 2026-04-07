e593ac5f7dfd5906c8f98812837471613f03507a
ShiChang Huang <1583924560@qq.com>
2026-04-07 18:54:22 +0800

    可视化优化了一些问题

--- src/qr_guide/launch/dog.launch.py
+++ src/qr_guide/launch/dog.launch.py
@@ -14,19 +14,151 @@ from launch_ros.parameter_descriptions import ParameterValue
 
 LEG_ORDER = ('FR', 'FL', 'RR', 'RL')
 LEG_COLOR_MATERIAL = {
-    'FR': 'accent_orange',
-    'FL': 'accent_cyan',
-    'RR': 'accent_green',
-    'RL': 'accent_pink',
+    'FR': 'metal_gray',
+    'FL': 'metal_gray',
+    'RR': 'metal_gray',
+    'RL': 'metal_gray',
 }
+JOINT_MATERIAL = 'metal_gray'
 
 
 def _format_xyz(values):
     return f'{values[0]:.6f} {values[1]:.6f} {values[2]:.6f}'
 
 
+def _capsule_visual(center_xyz, length, radius, axis, material):
+    if axis == 'x':
+        cylinder_rpy = '0 1.57079632679 0'
+        end_a = (-length / 2.0, 0.0, 0.0)
+        end_b = (length / 2.0, 0.0, 0.0)
+    elif axis == 'y':
+        cylinder_rpy = '1.57079632679 0 0'
+        end_a = (0.0, -length / 2.0, 0.0)
+        end_b = (0.0, length / 2.0, 0.0)
+    else:
+        cylinder_rpy = '0 0 0'
+        end_a = (0.0, 0.0, -length / 2.0)
+        end_b = (0.0, 0.0, length / 2.0)
+
+    cx, cy, cz = center_xyz
+    ax, ay, az = end_a
+    bx, by, bz = end_b
+
+    return f'''
+    <visual>
+      <origin xyz="{cx:.6f} {cy:.6f} {cz:.6f}" rpy="{cylinder_rpy}"/>
+      <geometry>
+        <cylinder radius="{radius:.6f}" length="{length:.6f}"/>
+      </geometry>
+      <material name="{material}"/>
+    </visual>
+    <visual>
+      <origin xyz="{cx + ax:.6f} {cy + ay:.6f} {cz + az:.6f}" rpy="0 0 0"/>
+      <geometry>
+        <sphere radius="{radius:.6f}"/>
+      </geometry>
+      <material name="{material}"/>
+    </visual>
+    <visual>
+      <origin xyz="{cx + bx:.6f} {cy + by:.6f} {cz + bz:.6f}" rpy="0 0 0"/>
+      <geometry>
+        <sphere radius="{radius:.6f}"/>
+      </geometry>
+      <material name="{material}"/>
+    </visual>'''
+
+
+def _joint_visual(origin_xyz, radius, material):
+    ox, oy, oz = origin_xyz
+    return f'''
+    <visual>
+      <origin xyz="{ox:.6f} {oy:.6f} {oz:.6f}" rpy="0 0 0"/>
+      <geometry>
+        <sphere radius="{radius:.6f}"/>
+      </geometry>
+      <material name="{material}"/>
+    </visual>'''
+
+
+def _box_visual(center_xyz, size_xyz, material):
+    cx, cy, cz = center_xyz
+    sx, sy, sz = size_xyz
+    return f'''
+    <visual>
+      <origin xyz="{cx:.6f} {cy:.6f} {cz:.6f}" rpy="0 0 0"/>
+      <geometry>
+        <box size="{sx:.6f} {sy:.6f} {sz:.6f}"/>
+      </geometry>
+      <material name="{material}"/>
+    </visual>'''
+
+
+def _cylinder_visual(center_xyz, radius, length, axis, material):
+    cx, cy, cz = center_xyz
+    if axis == 'x':
+        rpy = '0 1.57079632679 0'
+    elif axis == 'y':
+        rpy = '1.57079632679 0 0'
+    else:
+        rpy = '0 0 0'
+    return f'''
+    <visual>
+      <origin xyz="{cx:.6f} {cy:.6f} {cz:.6f}" rpy="{rpy}"/>
+      <geometry>
+        <cylinder radius="{radius:.6f}" length="{length:.6f}"/>
+      </geometry>
+      <material name="{material}"/>
+    </visual>'''
+
+
+def _build_body_visuals(robot_cfg):
+    body_x, body_y, body_z = robot_cfg['body_size_m']
+    upper_x = body_x * 0.72
+    upper_y = body_y * 0.70
+    upper_z = body_z * 0.44
+    lower_x = body_x * 0.62
+    lower_y = body_y * 0.44
+    lower_z = body_z * 0.24
+    side_radius = body_z * 0.18
+    shoulder_length = body_x * 0.70
+    front_bumper_x = body_x * 0.16
+    front_bumper_y = body_y * 0.50
+    front_bumper_z = body_z * 0.20
+    rear_pack_x = body_x * 0.18
+    rear_pack_y = body_y * 0.42
+    rear_pack_z = body_z * 0.18
+    lidar_radius = body_y * 0.10
+    lidar_height = body_z * 0.16
+    lidar_cap_radius = body_y * 0.13
+    lamp_radius = body_z * 0.065
+    camera_bar_x = body_x * 0.16
+    camera_bar_y = body_y * 0.26
+    camera_bar_z = body_z * 0.10
+
+    parts = []
+    parts.append(_box_visual((0.0, 0.0, 0.0), (upper_x, upper_y, upper_z), 'body_navy'))
+    parts.append(_cylinder_visual((0.0, body_y * 0.24, 0.0), side_radius, shoulder_length, 'x', 'body_navy'))
+    parts.append(_cylinder_visual((0.0, -body_y * 0.24, 0.0), side_radius, shoulder_length, 'x', 'body_navy'))
+    parts.append(_box_visual((0.0, 0.0, -body_z * 0.20), (lower_x, lower_y, lower_z), 'metal_gray'))
+    parts.append(_box_visual((body_x * 0.34, 0.0, body_z * 0.01),
+                             (front_bumper_x, front_bumper_y, front_bumper_z),
+                             'body_navy'))
+    parts.append(_box_visual((-body_x * 0.33, 0.0, -body_z * 0.01),
+                             (rear_pack_x, rear_pack_y, rear_pack_z),
+                             'body_navy'))
+    parts.append(_box_visual((body_x * 0.37, 0.0, body_z * 0.085),
+                             (camera_bar_x, camera_bar_y, camera_bar_z),
+                             'metal_gray'))
+    parts.append(_joint_visual((body_x * 0.39, body_y * 0.085, body_z * 0.082), lamp_radius, 'accent_cyan'))
+    parts.append(_joint_visual((body_x * 0.39, -body_y * 0.085, body_z * 0.082), lamp_radius, 'accent_cyan'))
+    parts.append(_cylinder_visual((0.0, 0.0, body_z * 0.34), lidar_radius, lidar_height, 'z', 'metal_gray'))
+    parts.append(_joint_visual((0.0, 0.0, body_z * 0.43), lidar_cap_radius, 'accent_green'))
+    return ''.join(parts)
+
+
 def _build_leg_block(leg_name, robot_cfg):
-    hip_mount = robot_cfg['hip_mounts_in_body'][leg_name]
+    hip_mount_raw = robot_cfg['hip_mounts_in_body'][leg_name]
+    hip_mount = [hip_mount_raw[0], -hip_mount_raw[1], hip_mount_raw[2]]
     l0 = robot_cfg['leg_geometry']['l0']
     l1 = robot_cfg['leg_geometry']['l1']
     l2 = robot_cfg['leg_geometry']['l2']
@@ -35,22 +167,23 @@ def _build_leg_block(leg_name, robot_cfg):
     is_front = leg_name in ('FR', 'FL')
     is_right = leg_name in ('FR', 'RR')
 
-    hip_axis_sign = -1.0 if is_front else 1.0
-    plane_offset_y = l0 if is_right else -l0
+    # 控制器内部仍然使用 y 向机器人右侧；这里单独反射到 RViz/URDF 的 y 向左可视化坐标。
+    hip_axis_sign = 1.0 if is_front else -1.0
+    plane_offset_y = -l0 if is_right else l0
     thigh_axis_sign = -1.0 if is_right else 1.0
     calf_axis_sign = 1.0 if is_right else -1.0
     hip_span = max(abs(plane_offset_y), 1e-3)
     material = LEG_COLOR_MATERIAL[leg_name]
+    hip_radius = 0.0105
+    rod_radius = 0.0088
+    joint_radius = 0.0130
+    foot_visual_radius = max(foot_radius * 0.78, 0.014)
 
     return f'''
   <link name="{leg_name}_hip_link">
-    <visual>
-      <origin xyz="0 {plane_offset_y / 2.0:.6f} 0" rpy="0 0 0"/>
-      <geometry>
-        <box size="0.032000 {hip_span:.6f} 0.032000"/>
-      </geometry>
-      <material name="{material}"/>
-    </visual>
+{_capsule_visual((0.0, plane_offset_y / 2.0, 0.0), hip_span, hip_radius, 'y', 'body_navy')}
+{_joint_visual((0.0, 0.0, 0.0), joint_radius, 'body_navy')}
+{_joint_visual((0.0, plane_offset_y, 0.0), joint_radius, JOINT_MATERIAL)}
   </link>
 
   <joint name="{leg_name}_hip" type="revolute">
@@ -62,13 +195,9 @@ def _build_leg_block(leg_name, robot_cfg):
   </joint>
 
   <link name="{leg_name}_thigh_link">
-    <visual>
-      <origin xyz="{l1 / 2.0:.6f} 0 0" rpy="0 0 0"/>
-      <geometry>
-        <box size="{l1:.6f} 0.045000 0.045000"/>
-      </geometry>
-      <material name="metal_gray"/>
-    </visual>
+{_capsule_visual((l1 / 2.0, 0.0, 0.0), l1, rod_radius, 'x', 'metal_gray')}
+{_joint_visual((0.0, 0.0, 0.0), joint_radius, 'body_navy')}
+{_joint_visual((l1, 0.0, 0.0), joint_radius, JOINT_MATERIAL)}
   </link>
 
   <joint name="{leg_name}_thigh" type="revolute">
@@ -80,13 +209,9 @@ def _build_leg_block(leg_name, robot_cfg):
   </joint>
 
   <link name="{leg_name}_calf_link">
-    <visual>
-      <origin xyz="{l2 / 2.0:.6f} 0 0" rpy="0 0 0"/>
-      <geometry>
-        <box size="{l2:.6f} 0.038000 0.038000"/>
-      </geometry>
-      <material name="metal_gray"/>
-    </visual>
+{_capsule_visual((l2 / 2.0, 0.0, 0.0), l2, rod_radius * 0.82, 'x', 'metal_gray')}
+{_joint_visual((0.0, 0.0, 0.0), joint_radius * 0.96, JOINT_MATERIAL)}
+{_joint_visual((l2, 0.0, 0.0), joint_radius * 0.90, 'foot_gold')}
   </link>
 
   <joint name="{leg_name}_calf" type="revolute">
@@ -101,7 +226,7 @@ def _build_leg_block(leg_name, robot_cfg):
     <visual>
       <origin xyz="0 0 0" rpy="0 0 0"/>
       <geometry>
-        <sphere radius="{foot_radius:.6f}"/>
+        <sphere radius="{foot_visual_radius:.6f}"/>
       </geometry>
       <material name="foot_gold"/>
     </visual>
@@ -124,9 +249,7 @@ def _build_robot_description(qr_guide_share):
 
     return template.format(
         robot_name=robot_cfg['name'],
-        body_x=f'{robot_cfg["body_size_m"][0]:.6f}',
-        body_y=f'{robot_cfg["body_size_m"][1]:.6f}',
-        body_z=f'{robot_cfg["body_size_m"][2]:.6f}',
+        body_visuals=_build_body_visuals(robot_cfg),
         leg_blocks=leg_blocks,
     )
 
